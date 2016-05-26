f81dea4925d19dc096a4c7f70733e20d04e00515
David G Chaves <davidgchaves@gmail.com>
2016-05-26 18:39:16 +0200

    Show a user

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -5,4 +5,9 @@ defmodule MysteryScienceTheater_3000.UserController do
     users = Repo.all(MysteryScienceTheater_3000.User)
     render conn, "index.html", users: users
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(MysteryScienceTheater_3000.User, id)
+    render conn, "show.html", user: user
+  end
 end
