a6a3571e32643f2cbb4f7740a8836fcc1c517789
David G Chaves <davidgchaves@gmail.com>
2016-05-28 13:04:14 +0200

    Add 'user has_many videos' relationship

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,6 +6,7 @@ defmodule MysteryScienceTheater_3000.User do
     field :username, :string
     field :password, :string, virtual: true
     field :password_hash, :string
+    has_many :videos, MysteryScienceTheater_3000.Video
 
     timestamps
   end
